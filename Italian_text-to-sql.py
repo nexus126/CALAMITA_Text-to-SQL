48dfe7d340ba7ebb2df51b8018d63f1cb957f674
Federico Ranaldi <65467500+nexus126@users.noreply.github.com>
2024-09-23 18:38:18 +0200

    Add files via upload

---
+++ Italian_text-to-sql.py
@@ -0,0 +1,13 @@
+#it's essential to refer to the full repository for a more comprehensive understanding of the dataset and evaluation methods.
+#in particular take a look at readme.md and \evaluation\instructions.txt
+
+OUTPUT_TYPE = "open-ended"
+
+#sql database dumps are available in the repository under Termite/databases/ folder and are named [no_insert_*db.sql] where *db is the name of a database
+PROMPT_1_TEMPLATE = """Considera il seguente database : {DUMP SQL}"""
+
+#nl query are retrievable in repository in Termite/queries.json where all natural language queries are stored
+PROMPT_2_TEMPLATE = """Traduci in SQL la seguente query in linguaggio naturale : {NL QUERY}"""
+
+#evaluation can be executed by running evaluation.py under \evaluation
+EVALUATION_METRIC = ["execution accurracy"]
